019beedfdc07d829989b518dce3012331cfd014d
Student User <student@workstation.lab.example.com>
2020-06-17 12:36:18 +0000

    make error

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n')
+  res.send('Hello World from pod: ' + process.envt.HOSTNAME + '\n')
 });
 
 app.listen(8080, function () {
